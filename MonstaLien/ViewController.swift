35932542178f22db79ba3538e40dc16447a1ba5e
Achmad Fathullah <fathullah@qiscus.co>
2021-04-30 08:46:56 +0700

    first commit

---
+++ MonstaLien/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MonstaLien
+//
+//  Created by zein rezky chandra on 28/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
